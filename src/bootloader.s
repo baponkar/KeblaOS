15d2110428f5725f30849d74ee71df2a3ab1ad49
Bapon Kar <baponkar600@gmail.com>
2024-09-08 15:53:02 +0530

    The source code of version 0.0.0.7 added

--- src/bootloader.s
+++ src/bootloader.s
@@ -1,66 +1,52 @@
 
 ; bootloader.s
-; Build Date : 07/09/2024
+; Build Date : 08/09/2024
 ; Last Update : 08/09/2024
 ; Developer : Mr. Bapon Kar
 ; Website : https://github.com/baponkar/KeblaOS
 ; Description :
 ; This Programm will read the disk and print the disk content
-;
+; 16 Bit Protected Mode
+; Version - 0.0.0.7
 ;
 
 
 
 [ORG 0x7C00]                        ; Shift Memory address 
 
-[BITS 16]
-
-section .text
-global main
-
-; Read some sector by using disk_load function 
+[BITS 32]
 
 
-main:
-    mov bp, 0x9000 ; set the stack safely away from us
-    mov sp, bp
+; Define some constants
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f
 
-    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
-    mov dh, 2 ; read 2 sectors
-    ; the bios sets 'dl' for our boot disk number
-    ; if you have trouble, use the '-fda' flag: 'qemu -fda file.bin'
-    call disk_load
+my_string:
+    db 'Tello',0
 
-    mov dx, [0x9000] ; retrieve the first loaded word, 0xdada
-    call print_hex
+; prints a null - terminated string pointed to by EDX
+print_string_pm :
+    pusha                   ; store all register condition in memory
+    mov edx , VIDEO_MEMORY  ; Set edx to the start of vid mem
 
-    call print_nl
+print_string_pm_loop :
+    mov ebx, my_string
+    mov al , [ebx] ; Store the char at EBX in AL
+    mov ah , WHITE_ON_BLACK ; Store the attributes in AH
+    cmp al , 0 ; if (al == 0) , at end of string , so
+    je print_string_pm_done ; jump to done
+    mov [edx] , ax ; Store char and attributes at current
+    ; character cell.
+    add ebx , 1 ; Increment EBX to the next char in string.
+    add edx , 2 ; Move to next character cell in vid mem.
+    jmp print_string_pm_loop ; loop around to print the next char.
 
-    mov dx, [0x9000 + 512] ; first word from second loaded sector, 0xface
-    call print_hex
+print_string_pm_done :
+    popa
+    ret ; Return from the function
 
-    jmp $
-
-
-
-
-%include "src/boot_sect_disk.asm"
-%include "src/boot_sect_print.asm"
-%include "src/boot_sect_print_hex.asm"
-
-
-BOOT_DRIVE:
-    db 0
 
 
 times 510 - ($-$$) db 0             ; Filling with zero
 dw 0xaa55                           ; Magic Number i.e. boot sector signature
 
-
-; We know that BIOS will load only the first 512 - byte sector from the disk ,
-; so if we purposely add a few more sectors to our code by repeating some
-; familiar numbers , we can prove to ourselfs that we actually loaded those
-; additional two sectors from the disk we booted from.
-
-times 256 dw 0 xdada    ; Sector 2 = 512 byte
-times 256 dw 0 xface    ; sector 3 = 512 byte 
