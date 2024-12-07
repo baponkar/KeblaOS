5fd72381279915cbdcc549b0fdb50e50f8ab4f64
Michael Petch <mpetch@gmail.com>
2024-12-07 13:41:50 -0700

    Fix GDT issues

--- source-code/x86_64/0.6/src/x86_64/gdt.h
+++ source-code/x86_64/0.6/src/x86_64/gdt.h
@@ -8,18 +8,15 @@
 
 
 struct gdt_entry
-{// 128 Bit
+{// 64-Bit
     uint16_t limit_low;       // Lower 16 bits of the segment limit
     uint16_t base_low;        // Lower 16 bits of the base address
     uint8_t base_middle;      // Next 8 bits of the base address
     uint8_t access;           // Access byte
     uint8_t granularity;      // Flags and upper limit
     uint8_t base_high;        // Next 8 bits of the base address
-    uint32_t base_upper;      // Upper 32 bits of the base address
-    uint32_t reserved;        // Reserved (must be zero)
 } __attribute__((packed));
 typedef struct gdt_entry gdt_entry_t;
-
 
 // Structure for GDTR
 struct gdtr {
