12c5bfe4e4341cea8e1b36b53d921ab9487e9c43
Bapon Kar <baponkar600@gmail.com>
2025-01-08 00:11:11 +0530

    updating makefile

--- src/util/util.h
+++ src/util/util.h
@@ -5,19 +5,6 @@
 #include <stddef.h>
 
 
-// Saved State in registers
-// typedef struct registers
-// {
-//     uint64_t rax, rbx, rcx, rdx, rbp, rsi, rdi;    // General-purpose registers
-//     uint64_t r8, r9, r10, r11, r12, r13, r14, r15; // General-purpose registers
-//     uint64_t gs, fs, es, ds;                       // Segment registers
-
-//     uint64_t int_no, err_code;                     // Interrupt number and error code (if applicable)
-//     uint64_t iret_rip, iret_cs, iret_rflags, iret_rsp, iret_ss; // CPU state
-// } registers_t;
-
-
-
 // Saved State in register
 typedef struct registers
 {
