f2df1f35be86845d8be3e096c078a6ef9829a5bd
Bapon Kar <baponkar600@gmail.com>
2025-02-04 22:34:10 +0530

    deleting unused directories

--- KeblaOS/src/include/arch/x86/disk.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <stdbool.h>
-#include <stddef.h>
-
-void get_disk_info();
--- KeblaOS/src/include/arch/x86/pit_timer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-#include "../../util/util.h"
-
-void init_timer(uint16_t interval);
-void timerHandler(registers_t *regs);
-void delay(uint32_t ms);
