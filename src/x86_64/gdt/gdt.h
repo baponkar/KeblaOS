a72b1104061c57d1262367c7ed77d022f3a6c7b6
Bapon Kar <baponkar600@gmail.com>
2025-03-15 03:41:46 +0530

    update in gdt

--- src/x86_64/gdt/gdt.h
+++ src/x86_64/gdt/gdt.h
@@ -6,6 +6,17 @@
 
 
 struct gdt_entry
+{// 64-Bit
+    uint16_t limit_low;       // Lower 16 bits of the segment limit
+    uint16_t base_low;        // Lower 16 bits of the base address
+    uint8_t base_middle;      // Next 8 bits of the base address
+    uint8_t access;           // Access byte
+    uint8_t granularity;      // Flags and upper limit
+    uint8_t base_high;        // Next 8 bits of the base address
+}__attribute__((packed, aligned(16)));
+typedef struct gdt_entry gdt_entry_t;
+
+struct tss_entry
 {// 128-Bit
     uint16_t limit_low;       // Lower 16 bits of the segment limit
     uint16_t base_low;        // Lower 16 bits of the base address
@@ -17,7 +28,7 @@ struct gdt_entry
     uint32_t base_upper;     // Upper 32 bits of the base address (for 64-bit TSS)
     uint32_t reserved;       // Reserved, must be zero
 }__attribute__((packed, aligned(16)));
-typedef struct gdt_entry gdt_entry_t;
+typedef struct tss_entry tss_entry_t;
 
 
 // Structure for GDTR
@@ -29,7 +40,7 @@ typedef struct gdtr gdtr_t;
 
 
 // Add TSS entry structure
-struct tss_entry{ // 9*64 = 576 bit = 72 byte = 0x48
+struct tss{ // 9*64 = 576 bit = 72 byte = 0x48
     uint32_t reserved0;
     uint64_t rsp0;  // Ring 0 Stack Pointer
     uint64_t rsp1;
@@ -42,13 +53,9 @@ struct tss_entry{ // 9*64 = 576 bit = 72 byte = 0x48
     uint16_t reserved3;
     uint16_t iopb_offset;
 } __attribute__((packed));
-typedef struct tss_entry tss_entry_t;
+typedef struct tss tss_t;
 
 
-
-
-void init_gdt_bootstrap_cpu();
-void init_tss_bootstrap_cpu();
 void start_bootstrap_gdt_tss();
 
 void init_gdt_ap_cpu(uint64_t core_id);
